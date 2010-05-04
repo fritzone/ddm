53c99740bee92bda24fcbaecd7685399f48e9bea
Ferenc Deak <fritzone@gmail.com>
2010-05-04 10:28:44 +0200

    First entry

---
+++ SerializableElement.h
@@ -0,0 +1,14 @@
+#ifndef _SERIALIZABLEELEMENT_H_
+#define _SERIALIZABLEELEMENT_H_
+
+
+/**
+ * All the classes that can be serialized should be derived from this class
+ */
+class SerializableElement
+{
+    void serialize(QDomDocument& doc, QDomElement& parent) const = 0;
+};
+
+#endif // _SERIALIZABLEELEMENT_H_
+
