2252a796106fee8b87f710b29888ff9358c43413
Ferenc Deak <fritzone@gmail.com>
2010-07-01 16:50:46 +0200

    some minor changes in the note window

--- EnterNoteTextDialog.cpp
+++ EnterNoteTextDialog.cpp
@@ -1,11 +1,28 @@
 #include "EnterNoteTextDialog.h"
 #include "ui_EnterNoteTextDialog.h"
 
+#include <QPainter>
+
 EnterNoteTextDialog::EnterNoteTextDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::EnterNoteTextDialog)
 {
     ui->setupUi(this);
+
+    //QPalette pal = ui->btnChangeColor->palette( );
+
+    QImage newImage(18, 18, QImage::Format_ARGB32);
+
+    QPainter painter(&newImage);
+
+    painter.fillRect(QRectF(0,0,18,18), Qt::white);
+    painter.fillRect( QRectF(2,14,14,4), QBrush(Qt::black));
+    painter.drawText(0,0,"FG");
+    painter.end();
+    QIcon icon(QPixmap::fromImage(newImage));
+
+    ui->btnChangeColor->setIcon(icon);
+
 }
 
 EnterNoteTextDialog::~EnterNoteTextDialog()
@@ -23,4 +40,9 @@ void EnterNoteTextDialog::changeEvent(QEvent *e)
     default:
         break;
     }
+}
+
+QString EnterNoteTextDialog::getText()
+{
+    return ui->textEdit->toHtml();
 }
