2033b71df8740438e2e778b617e6dc507a23057a
Ferenc <fritzone@gmail.com>
2010-08-01 19:39:39 +0200

    Small change

--- MajorVersion.cpp
+++ MajorVersion.cpp
@@ -9,11 +9,11 @@
 #include "Project.h"
 
 MajorVersion::MajorVersion(QTreeWidget* tree, QTreeWidget* dttree, ContextMenuEnabledTreeWidgetItem* projectItem, int ver, Project* p) :
-        tablesItem(0), m_project(p), tableInstancesItem(0),
+        tablesItem(0), tableInstancesItem(0),
         dtsItem(0), versionItem(0), diagramsItem(0), version(""), m_dataTypes(), m_tables(), m_diagrams(), m_tableInstances(),
         m_tree(tree), m_dtTree(dttree), m_projectItem(projectItem), m_tablePopupMenu(0),
         action_RemoveTable(0), action_DuplicateTable(0), action_SpecializeTable(0), action_InstantiateTable(0),
-        action_TableAddColumn(0)
+        action_TableAddColumn(0), m_project(p)
 {
     // make the dts sub item coming from the project
     QString v = QString::number(ver);
