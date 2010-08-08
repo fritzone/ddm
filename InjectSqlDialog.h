2fb3fc1b8566f3307d112d918c17a97cc98f9c61
Ferenc <fritzone@gmail.com>
2010-08-08 21:33:01 +0200

    little bit more SQL implementation

---
+++ InjectSqlDialog.h
@@ -0,0 +1,23 @@
+#ifndef INJECTSQLDIALOG_H
+#define INJECTSQLDIALOG_H
+
+#include <QDialog>
+
+namespace Ui {
+    class InjectSqlDialog;
+}
+
+class InjectSqlDialog : public QDialog {
+    Q_OBJECT
+public:
+    InjectSqlDialog(QWidget *parent = 0);
+    ~InjectSqlDialog();
+
+protected:
+    void changeEvent(QEvent *e);
+
+private:
+    Ui::InjectSqlDialog *ui;
+};
+
+#endif // INJECTSQLDIALOG_H
