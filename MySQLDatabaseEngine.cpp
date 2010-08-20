2d67839e5ecdd907f559e3d722db97b0231f72ff
Ferenc Deak <fritzone@gmail.com>
2010-08-20 16:29:17 +0200

    minor changes

--- MySQLDatabaseEngine.cpp
+++ MySQLDatabaseEngine.cpp
@@ -11,7 +11,7 @@ MySQLDatabaseEngine::MySQLDatabaseEngine() : DatabaseEngine("MySQL")
 {
 }
 
-bool MySQLDatabaseEngine::injectSql(const QString& host, const QString& user, const QString& pass, const QString& dbName, const QStringList& sqls)
+bool MySQLDatabaseEngine::injectSql(const QString& host, const QString& user, const QString& pass, const QString& dbName, const QStringList& sqls, QString& lastSql)
 {
     QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
     db.setHostName(host);
@@ -31,10 +31,14 @@ bool MySQLDatabaseEngine::injectSql(const QString& host, const QString& user, co
 
     for(int i=0; i<sqls.size(); i++)
     {
-        if(!query.exec(sqls[i]))
+        lastSql = sqls[i].trimmed();
+        if(lastSql.length() > 0)
         {
-            lastError = query.lastError().databaseText() + "/" + query.lastError().databaseText();
-            return false;
+            if(!query.exec(lastSql))
+            {
+                lastError = query.lastError().databaseText() + "/" + query.lastError().databaseText();
+                return false;
+            }
         }
     }
 
