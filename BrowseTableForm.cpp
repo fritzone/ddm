4ae9d731130fe0c84589a526638a1fc9736307e2
fritzone <fritzone@localhost>
2011-12-13 15:07:03 +0100

    some more code completion and bug fixes

--- BrowseTableForm.cpp
+++ BrowseTableForm.cpp
@@ -6,6 +6,11 @@
 #include <QSqlDriver>
 #include <QSqlError>
 #include <QSqlQuery>
+#include <QMessageBox>
+
+QTextEditWithCodeCompletion *BrowseTableForm::m_textEdit = 0;
+int BrowseTableForm::m_firstP = 0;
+int BrowseTableForm::m_lastP = 0;
 
 BrowseTableForm::BrowseTableForm(QWidget *parent, Connection* c, const QString& tab) :
     QWidget(parent),
@@ -13,10 +18,10 @@ BrowseTableForm::BrowseTableForm(QWidget *parent, Connection* c, const QString& 
 {
     ui->setupUi(this);
 
+    static QTextEditWithCodeCompletion *textEdit = new QTextEditWithCodeCompletion(this, c);
     ui->table->horizontalHeader()->setHighlightSections(true);
     ui->table->horizontalHeader()->setResizeMode(QHeaderView::Interactive);
 
-    textEdit = new QTextEditWithCodeCompletion(this, c);
     textEdit->setObjectName(QString::fromUtf8("textEdit"));
     textEdit->setBrowseForm(this);
     ui->verticalLayout_2->addWidget(textEdit);
@@ -33,6 +38,7 @@ BrowseTableForm::BrowseTableForm(QWidget *parent, Connection* c, const QString& 
 
     ui->table->setModel(model);
     ui->table->setEditTriggers(QAbstractItemView::DoubleClicked|QAbstractItemView::EditKeyPressed);
+    if(m_textEdit == 0) m_textEdit = textEdit;
 }
 
 BrowseTableForm::~BrowseTableForm()
@@ -60,6 +66,73 @@ QTableView* BrowseTableForm::getTable()
 void BrowseTableForm::onRunQuery()
 {
     QSqlQueryModel *model = new QSqlQueryModel(ui->table);
-    model->setQuery(QSqlQuery(textEdit->toPlainText(), m_connection->getQSqlDatabase()));
+    model->setQuery(QSqlQuery(retrieveCurrentQuery(), m_connection->getQSqlDatabase()));
+    if(model->lastError().type() != QSqlError::NoError)
+    {
+        QMessageBox::critical(this, tr("Error"), model->lastError().text(), QMessageBox::Ok);
+        return;
+    }
     ui->table->setModel(model);
+}
+
+QString BrowseTableForm::retrieveCurrentQuery()
+{
+    if(m_firstP != 0 || m_lastP != 0)
+    {
+        // TODO: This is duplicate with below
+        QTextCursor origCursor = m_textEdit->textCursor();
+        QTextCursor copyCursor = origCursor;
+
+        copyCursor.setPosition(m_firstP);
+        for(int j = m_firstP; j!= m_lastP; j++) copyCursor.movePosition(QTextCursor::Right, QTextCursor::KeepAnchor);
+        QTextCharFormat fmt = copyCursor.charFormat();
+        fmt.setBackground(QBrush(Qt::white));
+        copyCursor.setCharFormat(fmt);
+        m_textEdit->setTextCursor(origCursor);
+    }
+
+    QString t = m_textEdit->toPlainText();
+    int cursorP = m_textEdit->textCursor().position() - 1;
+    QString before = "";
+    QString after = "";
+    int i = cursorP;
+
+    // now go backward till the last ; or doc. beginning
+    while(i > -1 && t.at(i) != ';')
+    {
+        before.prepend(t.at(i));
+        i--;
+        if(i>-1)
+        {
+            if(t.at(i) == '\n' && t.at(i+1) == '\n') break;
+        }
+    }
+    m_firstP = i;
+    if(m_firstP == -1) m_firstP = 0;
+    // and then forward till the next ; or doc. end
+    i = cursorP+1;
+    while(i < t.length() && t.at(i) != ';')
+    {
+        after.append(t.at(i));
+        qDebug() << after;
+        i++;
+        if(i< t.length())
+        {
+            if(t.at(i) == '\n' && t.at(i-1) == '\n') break;
+        }
+    }
+    m_lastP = i;
+
+    QTextCursor origCursor = m_textEdit->textCursor();
+    QTextCursor copyCursor = origCursor;
+
+    copyCursor.setPosition(m_firstP);
+    for(int j = m_firstP; j!= m_lastP; j++) copyCursor.movePosition(QTextCursor::Right, QTextCursor::KeepAnchor);
+    QTextCharFormat fmt = copyCursor.charFormat();
+    fmt.setBackground(QBrush(Qt::white));
+    copyCursor.setCharFormat(fmt);
+    m_textEdit->setTextCursor(origCursor);
+
+
+    return before + after;
 }
