23f6b52e973670d61522199ad6efe43fade51cd9
fritzone <fritzone@localhost>
2012-02-16 14:15:59 +0100

    some redesigned new project dialog

--- NewProjectDialog.cpp
+++ NewProjectDialog.cpp
@@ -22,20 +22,6 @@ void NewProjectDialog::focusOnEditField() const
     m_ui->txtSolutionName->setFocus();
 }
 
-void NewProjectDialog::accept()
-{
-    if(m_ui->txtProjectName->text().size() > 0 && m_ui->txtSolutionName->text().size() > 0)
-    {
-        projectName = m_ui->txtProjectName->text();
-        solutionName = m_ui->txtSolutionName->text();
-        QDialog::accept();
-    }
-    else
-    {
-        QMessageBox::critical (this, tr("Error"), tr("Please specify a valid project and solution name"), QMessageBox::Ok);
-    }
-}
-
 void NewProjectDialog::changeEvent(QEvent *e)
 {
     QDialog::changeEvent(e);
@@ -66,8 +52,7 @@ bool NewProjectDialog::enableOOPFeatures() const
 
 int NewProjectDialog::getProjectType() const
 {
-    return 1;
-    //return (m_ui->cmbProjectType->currentIndex() + 1);
+    return (m_ui->listWidget->currentRow() + 1);
 }
 
 void NewProjectDialog::onHelp()
@@ -81,4 +66,23 @@ void NewProjectDialog::onHelp()
 bool NewProjectDialog::inheritDefaultDatatypes() const
 {
     return m_ui->chkInheritDefaultDatatypes->isChecked();
+}
+
+void NewProjectDialog::onReject()
+{
+    QDialog::reject();
+}
+
+void NewProjectDialog::onAccept()
+{
+    if(m_ui->txtProjectName->text().size() > 0 && m_ui->txtSolutionName->text().size() > 0)
+    {
+        projectName = m_ui->txtProjectName->text();
+        solutionName = m_ui->txtSolutionName->text();
+        QDialog::accept();
+    }
+    else
+    {
+        QMessageBox::critical (this, tr("Error"), tr("Please specify a valid project and solution name"), QMessageBox::Ok);
+    }
 }
