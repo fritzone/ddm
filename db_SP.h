a1e1b23a4393e6a8d223ff3bc1e841e415170bae
fritzone <fritzone@localhost>
2012-03-11 11:49:52 +0100

    some clarification for the sp engine

--- db_SP.h
+++ db_SP.h
@@ -11,25 +11,25 @@ class SpInstance;
 
 /**
  * This class represents a Special Property of a database object (Table, Column, Index, ...)
- * which will be:
- * 1. Presented in the GUI for each Database in a specific Tab
- * 2. The value from above will be inserted in the SQL query
+ * which will be presented in the GUI for each Database in a specific Tab
+ * When creating an object With Sps the instantiate method os being called to create the instances
+ * of the SPs for the given object.
  * Each Sp object has a property specifying where to put it in the query.
  */
 class Sp : public ObjectWithUid, public NamedItem
 {
 public:
 
-    Sp(const QString& sqlPropertyUid, const QString& dbObjectUid, const QString& propertyName, const QString& propertyGuiText);
+    Sp(const QString& sqlRoleUid, const QString& guiClassUid, const QString& referringObjectClassUid, const QString& propertyName, const QString& propertyGuiText);
 
     QString getDbObjectUid() const
     {
-        return m_destinationUid;
+        return m_guiClassUid;
     }
 
-    QString getSqlPropertyUid() const
+    QString getSqlRoleUid() const
     {
-        return m_sqlPropertyUid;
+        return m_sqlRoleUid;
     }
 
     QString getPropertyGuiText() const
@@ -39,15 +39,31 @@ public:
 
     virtual SpInstance* instantiate() = 0;
 
+    /**
+     * Returns the class UID of this. The class UID is NOT the one identifying the object in the GUI.
+     * This is a normal class UID.
+     */
     virtual QUuid getClassUid() const = 0;
+
+    /**
+     * This UID is the same uid as the object this SP refers to. For example this should return Table UID
+     * for properties referring to a table.
+     */
+    virtual QUuid getReferredObjectClassUid() const
+    {
+        return m_referredObjectClasUid;
+    }
 
 private:
 
     // this tells the sql query generator where to insert this property in the SQL query
-    QUuid m_sqlPropertyUid;
+    QUuid m_sqlRoleUid;
 
-    // this tells the app where to put this object in the GUI
-    QUuid m_destinationUid;
+    // this tells the app where to put this object in the GUI, ie. to which component this is referring
+    QUuid m_guiClassUid;
+
+    // this is the class UID of the "class" this SP ois referring to. For example: Table -> uidTable
+    QUuid m_referredObjectClasUid;
 
     // the property's text for the GUI
     QString m_propertyGuiText;
