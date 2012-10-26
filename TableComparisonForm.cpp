332543dde023a7145e855c30edd2c44e8f7d0e27
fritzone <fritzone@localhost>
2012-10-26 15:00:46 +0200

    added some stuff for table comparison

--- TableComparisonForm.cpp
+++ TableComparisonForm.cpp
@@ -2,7 +2,7 @@
 #include "ui_TableComparisonForm.h"
 #include "core_Table.h"
 
-TableComparisonForm::TableComparisonForm(QWidget *parent) :
+TableComparisonForm::TableComparisonForm(QWidget *parent) : m_leftTable(0), m_rightTable(0),
     QWidget(parent),
     ui(new Ui::TableComparisonForm)
 {
@@ -12,6 +12,9 @@ TableComparisonForm::TableComparisonForm(QWidget *parent) :
     m_spl->addWidget(ui->grpLeft);
     m_spl->addWidget(ui->grpRight);
     ui->horizontalLayout->addWidget(m_spl);
+
+    m_trees[LEFT] = ui->treeWidgetLeft;
+    m_trees[RIGHT] = ui->treeWidgetRight;
 }
 
 TableComparisonForm::~TableComparisonForm()
@@ -24,4 +27,14 @@ void TableComparisonForm::populateTree()
     ui->txtTab1->setText(m_leftTable->getName());
     if(m_rightTable) ui->txtTab2->setText(m_rightTable->getName());
     else ui->txtTab2->setText("Deleted");
+
+    populateColumns();
+}
+
+void TableComparisonForm::populateColumns()
+{
+    QTreeWidgetItem* colItemLeft = new QTreeWidgetItem(QStringList("Columns"));
+    m_trees[LEFT]->addTopLevelItem(colItemLeft);
+    QTreeWidgetItem* colItemRight = new QTreeWidgetItem(QStringList("Columns"));
+    m_trees[RIGHT]->addTopLevelItem(colItemRight);
 }
